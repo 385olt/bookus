c947123a63d95cd8bf1c951c1a08e0723c73b8cc
385 <jackbv95@gmail.com>
2015-12-13 00:49:26 +0200

    done with LoginManager, ready for OAuth

--- app/controllers.py
+++ app/controllers.py
@@ -1,11 +1,21 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm
 from .forms import LoginForm
+from .models import User
+
+@lm.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
+@app.before_request
+def before_request():
+	g.user = current_user
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username': 'Miguel'} # fake user
+	user = g.user
 	books = [
 				{
 					'author': {'username': 'Jack'},
@@ -20,11 +30,17 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if g.user is not None and g.user.is_authenticated:
+		return redirect(url_for('index'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('Login requested for OpenID="%s", remeber_me=%s' %
-			(form.openid.data, str(form.remember_me.data)))
-		return redirect('/index')
+		session['remember_me'] = form.remember_me.data
+		return redirect(url_for('index'))
 	return render_template('login.html',
 							form=form,
 							providers=app.config['OPENID_PROVIDERS'])
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
